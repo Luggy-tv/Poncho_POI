2c54821d45e053dd6ed7b11911353a31db4aa209
luisAlfonso <renumoo117@gmail.com>
2023-09-10 20:04:07 -0600

    Initial commit

---
+++ chatapp.sql
@@ -0,0 +1,24 @@
+create database if not exists chatapp;
+use chatapp;
+
+CREATE TABLE IF NOT EXISTS `messages` (
+  `msg_id` int NOT NULL AUTO_INCREMENT,
+  `incoming_msg_id` int NOT NULL,
+  `outgoing_msg_id` int NOT NULL,
+  `msg` varchar(1000) NOT NULL,
+  constraint PK_msg
+	primary key (msg_id)
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `unique_id` int NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `img` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  constraint PK_user
+	primary key (user_id)
+) ENGINE=InnoDB;
---
+++ index.php
@@ -0,0 +1,50 @@
+<?php 
+  session_start();
+  if(isset($_SESSION['unique_id'])){
+    header("location: users.php");
+  }
+?>
+
+<?php include_once "header.php"; ?>
+<body>
+  <div class="wrapper">
+    <section class="form signup">
+      <header>Realtime Chat App</header>
+      <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
+        <div class="error-text"></div>
+        <div class="name-details">
+          <div class="field input">
+            <label>First Name</label>
+            <input type="text" name="fname" placeholder="Nombres" required>
+          </div>
+          <div class="field input">
+            <label>Last Name</label>
+            <input type="text" name="lname" placeholder="Apellidos" required>
+          </div>
+        </div>
+        <div class="field input">
+          <label>Email Address</label>
+          <input type="text" name="email" placeholder="Correo" required>
+        </div>
+        <div class="field input">
+          <label>Password</label>
+          <input type="password" name="password" placeholder="Contraseña" required>
+          <i class="fas fa-eye"></i>
+        </div>
+        <div class="field image">
+          <label>Select Image</label>
+          <input type="file" name="image" accept="image/x-png,image/gif,image/jpeg,image/jpg" required>
+        </div>
+        <div class="field button">
+          <input type="submit" name="submit" value="Crear usuario">
+        </div>
+      </form>
+      <div class="link">Ya tienes cuenta? <a href="login.php">Inicia sesion</a></div>
+    </section>
+  </div>
+
+  <script src="javascript/pass-show-hide.js"></script>
+  <script src="javascript/signup.js"></script>
+
+</body>
+</html>
