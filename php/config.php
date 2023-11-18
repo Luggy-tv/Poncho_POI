e9c47c2b2d62dc4e0c3159d2baafec74e66ceb11
luisAlfonso <Luisalfonsomartinezmartinez@outlook.es>
2023-11-17 22:54:01 -0600

    Cambios en la configuracion de la bd

--- php/config.php
+++ php/config.php
@@ -1,11 +1,20 @@
 <?php
-  $hostname = "localhost";
-  $username = "root";
-  $password = "";
-  $dbname = "chatapp";
 
-  $conn = mysqli_connect($hostname, $username, $password, $dbname);
-  if(!$conn){
-    echo "Database connection error".mysqli_connect_error();
-  }
+session_start();
+
+$DB_HOST = $_ENV["DB_HOST"];
+$DB_USER = $_ENV["DB_USER"];
+$DB_PASSWORD = $_ENV["DB_PASSWORD"];
+$DB_NAME = $_ENV["DB_NAME"];
+$DB_PORT = $_ENV["DB_PORT"];
+
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$dbname = "chatapp";
+
+$conn = mysqli_connect($hostname, $username, $password, $dbname);
+if (!$conn) {
+  echo "Database connection error" . mysqli_connect_error();
+}
 ?>
